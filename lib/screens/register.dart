5461adf18bd0ab7ba25ee4cd08fbf58bcbdb0ed5
Arjun Narayanan <arjun1408@gmail.com>
2020-11-17 20:28:06 +0530

    add register

---
+++ lib/screens/register.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:housingsociety/screens/wrapper.dart';
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:housingsociety/screens/register.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Register();
   }
 }
