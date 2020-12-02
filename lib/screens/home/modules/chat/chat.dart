d4bf65cbaecaffc9699eef7a50cde6fbc7ec9f58
Arjun Narayanan <arjun14081999@gmail.com>
2020-12-02 22:00:59 +0530

    add chat ui

--- lib/screens/home/modules/chat/chat.dart
+++ lib/screens/home/modules/chat/chat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:housingsociety/shared/constants.dart';
 
 class Chat extends StatefulWidget {
   @override
@@ -6,8 +7,68 @@ class Chat extends StatefulWidget {
 }
 
 class _ChatState extends State<Chat> {
+  String message;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat'),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              color: Colors.white,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 10,
+                  child: Form(
+                    child: TextFormField(
+                      onChanged: (val) {
+                        message = val;
+                      },
+                      decoration: InputDecoration(
+                        prefixIcon: IconButton(
+                          onPressed: () {
+                            print('clicked');
+                          },
+                          icon: Icon(
+                            Icons.emoji_emotions,
+                          ),
+                        ),
+                        fillColor: kSpaceCadet,
+                        filled: true,
+                        hintText: 'Type a message...',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(30.0),
+                          ),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: IconButton(
+                    color: kAmaranth,
+                    onPressed: () {
+                      print('button');
+                    },
+                    icon: Icon(Icons.send),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 8.0,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -4,3 +4,4 @@ const Color kAmaranth = Color(0xFFEB1555);
 const Color kMediumAquamarine = Color(0xFF14EBAA);
 const Color kRussianViolet = Color(0xFF0C0A3E);
 const Color kOxfordBlue = Color(0xFF111328);
+const Color kSpaceCadet = Color(0xFF1D1E33);
