d56560e45f375a40eb3e245e57fb44bd6c3a377b
Arjun Narayanan <arjun14081999@gmail.com>
2020-12-25 22:15:39 +0530

    add dynamicparticipants

--- lib/screens/home/modules/voting/addvoting.dart
+++ lib/screens/home/modules/voting/addvoting.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:housingsociety/screens/home/modules/voting/dynamicparticipants.dart';
 
-class AddVoting extends StatelessWidget {
+class AddVoting extends StatefulWidget {
   static const String id = 'add_voting';
+
+  @override
+  _AddVotingState createState() => _AddVotingState();
+}
+
+class _AddVotingState extends State<AddVoting> {
+  List<DynamicParticipants> dynamicparticipants = [];
+
+  void addParticpant() {
+    setState(() {
+      dynamicparticipants.add(DynamicParticipants());
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => addParticpant(),
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: ListView.builder(
+          itemCount: dynamicparticipants.length,
+          itemBuilder: (context, index) {
+            return Column(children: [
+              dynamicparticipants[index],
+            ]);
+          },
+        ),
+      ),
     );
   }
 }
---
+++ lib/screens/home/modules/voting/dynamicparticipants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DynamicParticipants extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        labelText: 'Participant 1',
+      ),
+    );
+  }
+}
