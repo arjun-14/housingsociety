e621dc6e2cd2f141cc85140365dbaab78f944682
Arjun Narayanan <arjun14081999@gmail.com>
2021-01-07 22:22:35 +0530

    contacts: add bottom nav bar & initialize emergencycontacts

--- lib/screens/home/modules/contacts/contacts.dart
+++ lib/screens/home/modules/contacts/contacts.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:housingsociety/screens/home/modules/contacts/emergencycontacts.dart';
+import 'package:housingsociety/shared/constants.dart';
 import 'package:housingsociety/shared/loading.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class Contacts extends StatelessWidget {
+class Contacts extends StatefulWidget {
   static const String id = 'contacts';
+
+  @override
+  _ContactsState createState() => _ContactsState();
+}
+
+class _ContactsState extends State<Contacts> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,40 +29,66 @@ class Contacts extends StatelessWidget {
       appBar: AppBar(
         title: Text('Contacts'),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: userProfile.snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text('Something went wrong');
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Loading();
-          }
-          return ListView(
-            children: snapshot.data.docs.map((DocumentSnapshot document) {
-              return ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: document.data()['profile_picture'] == ' '
-                      ? AssetImage('assets/images/default_profile_pic.jpg')
-                      : NetworkImage(document.data()['profile_picture']),
-                ),
-                title: Text(
-                  document.data()['name'],
-                ),
-                subtitle: Text(
-                  document.data()['wing'] + ' ' + document.data()['flatno'],
-                ),
-                trailing: IconButton(
-                  icon: Icon(Icons.call),
-                  onPressed: () {
-                    print("tel://" + document.data()['phone_no']);
-                    launch("tel://" + document.data()['phone_no']);
-                  },
-                ),
-              );
-            }).toList(),
-          );
-        },
+      body: _selectedIndex == 0
+          ? StreamBuilder<QuerySnapshot>(
+              stream: userProfile.snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Something went wrong');
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Loading();
+                }
+                return ListView(
+                  children: snapshot.data.docs.map((DocumentSnapshot document) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: document.data()['profile_picture'] ==
+                                ' '
+                            ? AssetImage(
+                                'assets/images/default_profile_pic.jpg')
+                            : NetworkImage(document.data()['profile_picture']),
+                      ),
+                      title: Text(
+                        document.data()['name'],
+                      ),
+                      subtitle: Text(
+                        document.data()['wing'] +
+                            ' ' +
+                            document.data()['flatno'],
+                      ),
+                      trailing: IconButton(
+                        color: kAmaranth,
+                        icon: Icon(Icons.call),
+                        onPressed: () {
+                          launch("tel://" + document.data()['phone_no']);
+                        },
+                      ),
+                    );
+                  }).toList(),
+                );
+              },
+            )
+          : EmergencyContacts(),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: kXiketic,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.call,
+            ),
+            label: 'Contacts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.info,
+            ),
+            label: 'Emergency',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
