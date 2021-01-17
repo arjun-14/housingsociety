f631a71db63b0e0450fdc082a9ae1a3e024a5789
Arjun Narayanan <arjun14081999@gmail.com>
2021-01-17 23:29:39 +0530

    health: individual status ui

--- lib/screens/home/modules/health/health.dart
+++ lib/screens/home/modules/health/health.dart
@@ -1,11 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:housingsociety/shared/constants.dart';
 
-class Health extends StatelessWidget {
+class Health extends StatefulWidget {
   static const String id = 'health';
+
+  @override
+  _HealthState createState() => _HealthState();
+}
+
+class _HealthState extends State<Health> {
+  String groupvalue = 'Healthy';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('Status'),
+      ),
+      body: Column(
+        children: [
+          RadioListTile(
+            title: Text('Home Quarantined'),
+            value: 'Home Quarantined',
+            groupValue: groupvalue,
+            onChanged: (value) {
+              setState(() {
+                groupvalue = value;
+              });
+            },
+          ),
+          RadioListTile(
+            title: Text('Hospitalized'),
+            value: 'Hospitalized',
+            groupValue: groupvalue,
+            onChanged: (value) {
+              setState(() {
+                groupvalue = value;
+              });
+            },
+          ),
+          RadioListTile(
+            title: Text('Healthy'),
+            value: 'Healthy',
+            groupValue: groupvalue,
+            onChanged: (value) {
+              setState(() {
+                groupvalue = value;
+              });
+            },
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: kXiketic,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            label: 'Status',
+            icon: Icon(
+              Icons.medical_services,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: 'Summary',
+            icon: Icon(
+              Icons.info,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.save,
+        ),
+      ),
     );
   }
 }
