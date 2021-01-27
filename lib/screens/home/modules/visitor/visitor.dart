e21e454be1880cbe4b986f7fdb53d7e1abe0cfc7
Arjun Narayanan <arjun14081999@gmail.com>
2021-01-27 22:51:59 +0530

    visitor: tried showalert on dropdown clear history

--- lib/screens/home/modules/visitor/visitor.dart
+++ lib/screens/home/modules/visitor/visitor.dart
@@ -1,14 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:housingsociety/screens/home/modules/visitor/addvisitor.dart';
 import 'package:housingsociety/screens/home/modules/visitor/realtimevisitorupdate.dart';
+import 'package:housingsociety/shared/constants.dart';
 
-class Visitor extends StatelessWidget {
+class Visitor extends StatefulWidget {
   static const String id = 'visitor';
+
+  @override
+  _VisitorState createState() => _VisitorState();
+}
+
+class _VisitorState extends State<Visitor> {
+  // String dropdownValue = 'Clear History';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Visitor History'),
+        actions: [
+          DropdownButton(
+            // value: dropdownValue,
+            icon: Icon(
+              Icons.more_vert,
+              color: Colors.white,
+            ),
+            underline: Container(
+              color: kXiketic,
+            ),
+            items: [
+              DropdownMenuItem(
+                value: 'Clear History',
+                child: Text('Clear History'),
+                onTap: () {
+                  print('clicked');
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text(
+                            'Are you sure you want to clear visitor history?'),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('Yes'),
+                          )
+                        ],
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
+            onChanged: (value) {},
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
