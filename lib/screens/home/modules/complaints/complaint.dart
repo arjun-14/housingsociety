ed552d76453107d76258ddfadbffde3243953b32
Arjun Narayanan <arjun14081999@gmail.com>
2021-03-09 22:50:50 +0530

    complaints : replace nlp python section with rake

--- lib/screens/home/modules/complaints/complaint.dart
+++ lib/screens/home/modules/complaints/complaint.dart
@@ -5,6 +5,7 @@ import 'package:housingsociety/screens/home/modules/complaints/addcomplaint.dart
 import 'package:housingsociety/screens/home/modules/complaints/realtimecomplaintupdate.dart';
 import 'package:housingsociety/shared/constants.dart';
 import 'package:http/http.dart' as http;
+import 'package:rake/rake.dart';
 
 class Complaint extends StatelessWidget {
   static const String id = 'complaint';
@@ -16,31 +17,20 @@ class Complaint extends StatelessWidget {
         title: Text('Complaints'),
         actions: [
           TextButton(
-            onPressed: () async {
-              final url = 'https://192.168.0.107/';
-              final response = await http.post(url,
-                  body: json.encode({
-                    'complaint':
-                        'electricity is not working. we have water shortage'
-                  }));
-              print('response' + response.statusCode.toString());
+            onPressed: () {
+              final exampleText = "The lift in c-wing is not working.";
+              final rake = Rake();
+             
+              print(rake.rank(exampleText));
+
+            
             },
             child: Text(
               'Analyze',
               style: TextStyle(color: kAmaranth),
             ),
           ),
-          TextButton(
-            onPressed: () async {
-              final url = 'https://192.168.0.107/';
-              final response = await http.get(url);
-              print(json.decode(response.body));
-            },
-            child: Text(
-              'GET',
-              style: TextStyle(color: kAmaranth),
-            ),
-          ),
+          
         ],
       ),
       floatingActionButton: FloatingActionButton(
