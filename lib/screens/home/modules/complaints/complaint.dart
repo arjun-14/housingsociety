d63c951bb0780f557d8ed04d59bf8cbb723a17a4
Arjun Narayanan <arjun14081999@gmail.com>
2021-03-11 13:41:53 +0530

    complaints: update pie chart as per user input

--- lib/screens/home/modules/complaints/complaint.dart
+++ lib/screens/home/modules/complaints/complaint.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:housingsociety/screens/home/modules/complaints/addcomplaint.dart';
+import 'package:housingsociety/screens/home/modules/complaints/piechart.dart';
 import 'package:housingsociety/screens/home/modules/complaints/realtimecomplaintupdate.dart';
 import 'package:housingsociety/shared/constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:pie_chart/pie_chart.dart';
 import 'package:rake/rake.dart';
 
 class Complaint extends StatelessWidget {
   static const String id = 'complaint';
 
   Widget build(BuildContext context) {
-    Map<String, int> keywords = {};
+    Map<String, double> keywords = {};
     return Scaffold(
       appBar: AppBar(
         title: Text('Complaints'),
@@ -30,11 +30,35 @@ class Complaint extends StatelessWidget {
                     if (keywords.containsKey(keyword)) {
                       keywords[keyword] += 1;
                     } else {
-                      keywords[keyword] = 0;
+                      keywords[keyword] = 1;
                     }
                   }
                 });
               });
+              keywords.isEmpty
+                  ? showDialog(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        content: Text(
+                            'There should be atleast one complaint to analyse'),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('Ok'),
+                          )
+                        ],
+                      ),
+                    )
+                  : Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PieChartComplaints(
+                          dataMap: keywords,
+                        ),
+                      ),
+                    );
               print(keywords);
             },
             child: Text(
---
+++ lib/screens/home/modules/complaints/piechart.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:housingsociety/shared/constants.dart';
+import 'package:pie_chart/pie_chart.dart';
+
+class PieChartComplaints extends StatefulWidget {
+  final Map<String, double> dataMap;
+  PieChartComplaints({this.dataMap});
+
+  @override
+  _PieChartComplaintsState createState() => _PieChartComplaintsState();
+}
+
+class _PieChartComplaintsState extends State<PieChartComplaints> {
+  dynamic noOfProblems;
+  Map<String, double> newDataMap = {};
+  @override
+  void initState() {
+    super.initState();
+    newDataMap = widget.dataMap;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pie Chart'),
+      ),
+      body: ListView(
+        children: [
+          Container(
+            color: kXiketic,
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          decoration:
+                              InputDecoration(labelText: 'No of problems'),
+                          keyboardType: TextInputType.number,
+                          onChanged: (value) {
+                            setState(() {
+                              noOfProblems = value;
+                            });
+                          },
+                        ),
+                      ),
+                      Builder(
+                        builder: (BuildContext context) => TextButton(
+                          onPressed: () {
+                            if (int.parse(noOfProblems) < 1 ||
+                                int.parse(noOfProblems) >
+                                    widget.dataMap.length) {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) => AlertDialog(
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text('Okay'),
+                                    )
+                                  ],
+                                  content: Text(
+                                      'No of problems must be in the range 1-${widget.dataMap.length}'),
+                                ),
+                              );
+                              // final snackBar = SnackBar(
+                              //   content: Text(
+                              //       'No of problems must be in the range 1-${widget.dataMap.length}'),
+                              // );
+                              // ScaffoldMessenger.of(context)
+                              //     .showSnackBar(snackBar);
+                            } else {
+                              newDataMap = {};
+                              dynamic keys = widget.dataMap.keys;
+                              print(keys);
+
+                              for (var i = 0;
+                                  i < int.parse(noOfProblems);
+                                  i++) {
+                                setState(() {
+                                  newDataMap[keys.elementAt(i)] =
+                                      widget.dataMap[keys.elementAt(i)];
+                                });
+                              }
+                              print(newDataMap);
+                            }
+                          },
+                          child: Text('Update'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                PieChart(
+                  dataMap: newDataMap,
+                  animationDuration: Duration(milliseconds: 800),
+                  chartRadius: MediaQuery.of(context).size.width / 1.5,
+                  legendOptions: LegendOptions(
+                    showLegendsInRow: true,
+                    legendPosition: LegendPosition.bottom,
+                    showLegends: true,
+                    legendShape: BoxShape.rectangle,
+                    legendTextStyle: TextStyle(
+                      decoration: TextDecoration.none,
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                  chartValuesOptions: ChartValuesOptions(
+                    showChartValueBackground: true,
+                    showChartValues: true,
+                    showChartValuesInPercentage: true,
+                    showChartValuesOutside: true,
+                    decimalPlaces: 1,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
