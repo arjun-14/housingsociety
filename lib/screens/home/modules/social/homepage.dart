bfcc3547307718d70af578bfb0d3f20e6a2483e1
Arjun Narayanan <arjun14081999@gmail.com>
2021-04-28 01:14:36 +0530

    social: refactor

--- lib/screens/home/modules/social/homepage.dart
+++ lib/screens/home/modules/social/homepage.dart
@@ -1,13 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:housingsociety/models/user.dart';
-import 'package:housingsociety/screens/home/modules/complaints/comments.dart';
+import 'package:housingsociety/screens/home/modules/social/reusableposttile.dart';
 import 'package:housingsociety/services/auth.dart';
-import 'package:housingsociety/services/database.dart';
-import 'package:housingsociety/shared/constants.dart';
 import 'package:housingsociety/shared/loading.dart';
 import 'package:provider/provider.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class HomePageSocial extends StatefulWidget {
   @override
@@ -57,8 +54,6 @@ class _HomePageSocialState extends State<HomePageSocial> {
         .collection('module_social')
         .doc(user.uid)
         .collection('following');
-    CollectionReference moduleSocialPhotos_ =
-        FirebaseFirestore.instance.collection('module_social_photos');
 
     return StreamBuilder(
       stream: moduleSocial.snapshots(),
@@ -76,12 +71,12 @@ class _HomePageSocialState extends State<HomePageSocial> {
           usersFollowed.add(user.data()['uid']);
         });
 
-        Query moduleSocialPhotos = FirebaseFirestore.instance
+        Query moduleSocialPhotosCurrentUser = FirebaseFirestore.instance
             .collection('module_social_photos')
             .where('uid', whereIn: usersFollowed)
             .orderBy('timestamp', descending: true);
         return StreamBuilder(
-          stream: moduleSocialPhotos.snapshots(),
+          stream: moduleSocialPhotosCurrentUser.snapshots(),
           builder: (BuildContext context,
               AsyncSnapshot<QuerySnapshot> photosSnapshot) {
             if (photosSnapshot.hasError) {
@@ -93,115 +88,9 @@ class _HomePageSocialState extends State<HomePageSocial> {
             return ListView(
               children:
                   photosSnapshot.data.docs.map((DocumentSnapshot document) {
-                return Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ListTile(
-                        leading: CircleAvatar(
-                          backgroundImage:
-                              document.data()['profile_picture'] == null
-                                  ? AssetImage(
-                                      'assets/images/default_profile_pic.jpg')
-                                  : NetworkImage(
-                                      document.data()['profile_picture'],
-                                    ),
-                        ),
-                        title: Text(document.data()['username']),
-                      ),
-                      GestureDetector(
-                        onDoubleTap: () {
-                          likes.containsKey(document.id)
-                              ? likes[document.id] = !likes[document.id]
-                              : likes[document.id] = true;
-                          DatabaseService().updateLikes(
-                            moduleSocialPhotos_,
-                            moduleSocialPhotosLikes,
-                            document.id,
-                            document.data()['likes'],
-                            user.uid,
-                          );
-                        },
-                        child: Image.network(
-                          document.data()['url'],
-                          loadingBuilder: (context, child, progress) {
-                            return progress == null ? child : Loading();
-                          },
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          IconButton(
-                              icon: Icon(
-                                likes.containsKey(document.id)
-                                    ? likes[document.id] == true
-                                        ? Icons.favorite
-                                        : Icons.favorite_border
-                                    : Icons.favorite_border,
-                                color: kAmaranth,
-                              ),
-                              onPressed: () {
-                                likes.containsKey(document.id)
-                                    ? likes[document.id] = !likes[document.id]
-                                    : likes[document.id] = true;
-
-                                DatabaseService().updateLikes(
-                                  moduleSocialPhotos_,
-                                  moduleSocialPhotosLikes,
-                                  document.id,
-                                  document.data()['likes'],
-                                  user.uid,
-                                );
-                              }),
-                          IconButton(
-                              icon: Icon(
-                                Icons.chat_bubble_outline,
-                                color: kAmaranth,
-                              ),
-                              onPressed: () {
-                                showBarModalBottomSheet(
-                                    context: context,
-                                    builder: (context) => Comments(
-                                          social: true,
-                                          docid: document.id,
-                                          socialusername:
-                                              document.data()['username'],
-                                        ));
-                              }),
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 12),
-                        child: Text(
-                          document.data()['likes'].toString() + ' likes',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 17.0,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                        child: RichText(
-                          text: TextSpan(
-                              style: TextStyle(
-                                fontSize: 17.0,
-                              ),
-                              children: [
-                                TextSpan(
-                                  text: document.data()['username'],
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                                TextSpan(
-                                    text: '  ' + document.data()['caption'])
-                              ]),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      )
-                    ],
-                  ),
+                return ReusablePostDisplayTile(
+                  document: document,
+                  likes: likes,
                 );
               }).toList(),
             );
---
+++ lib/screens/home/modules/social/reusableposttile.dart
@@ -0,0 +1,129 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:housingsociety/models/user.dart';
+import 'package:housingsociety/screens/home/modules/complaints/comments.dart';
+import 'package:housingsociety/services/database.dart';
+import 'package:housingsociety/shared/constants.dart';
+import 'package:housingsociety/shared/loading.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:provider/provider.dart';
+
+class ReusablePostDisplayTile extends StatelessWidget {
+  final DocumentSnapshot document;
+  final Map likes;
+  ReusablePostDisplayTile({this.document, this.likes});
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<CurrentUser>(context);
+    CollectionReference moduleSocialPhotos =
+        FirebaseFirestore.instance.collection('module_social_photos');
+    CollectionReference moduleSocialPhotosLikes =
+        FirebaseFirestore.instance.collection('module_social_photos_likes');
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: document.data()['profile_picture'] == null
+                  ? AssetImage('assets/images/default_profile_pic.jpg')
+                  : NetworkImage(
+                      document.data()['profile_picture'],
+                    ),
+            ),
+            title: Text(document.data()['username']),
+          ),
+          GestureDetector(
+            onDoubleTap: () {
+              likes.containsKey(document.id)
+                  ? likes[document.id] = !likes[document.id]
+                  : likes[document.id] = true;
+              DatabaseService().updateLikes(
+                moduleSocialPhotos,
+                moduleSocialPhotosLikes,
+                document.id,
+                document.data()['likes'],
+                user.uid,
+              );
+            },
+            child: Image.network(
+              document.data()['url'],
+              loadingBuilder: (context, child, progress) {
+                return progress == null ? child : Loading();
+              },
+            ),
+          ),
+          Row(
+            children: [
+              IconButton(
+                  icon: Icon(
+                    likes.containsKey(document.id)
+                        ? likes[document.id] == true
+                            ? Icons.favorite
+                            : Icons.favorite_border
+                        : Icons.favorite_border,
+                    color: kAmaranth,
+                  ),
+                  onPressed: () {
+                    likes.containsKey(document.id)
+                        ? likes[document.id] = !likes[document.id]
+                        : likes[document.id] = true;
+
+                    DatabaseService().updateLikes(
+                      moduleSocialPhotos,
+                      moduleSocialPhotosLikes,
+                      document.id,
+                      document.data()['likes'],
+                      user.uid,
+                    );
+                  }),
+              IconButton(
+                  icon: Icon(
+                    Icons.chat_bubble_outline,
+                    color: kAmaranth,
+                  ),
+                  onPressed: () {
+                    showBarModalBottomSheet(
+                        context: context,
+                        builder: (context) => Comments(
+                              social: true,
+                              docid: document.id,
+                              socialusername: document.data()['username'],
+                            ));
+                  }),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Text(
+              document.data()['likes'].toString() + ' likes',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 17.0,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: RichText(
+              text: TextSpan(
+                  style: TextStyle(
+                    fontSize: 17.0,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: document.data()['username'],
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(text: '  ' + document.data()['caption'])
+                  ]),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          )
+        ],
+      ),
+    );
+  }
+}
